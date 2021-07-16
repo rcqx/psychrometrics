020b8ce705fd2e3c6470da1761b83a4bfb4f32a9
jr-cast <castanos@pm.me>
2021-07-16 16:34:41 +0100

    update

---
+++ 1_psychrometrics/psy_exercise/psychrolib_test.py
@@ -0,0 +1,15 @@
+from os import closerange
+import psychrolib, psychrochart
+psychrolib.SetUnitSystem(psychrolib.SI)
+
+# psychrolib methods testing  
+TDewPoint = psychrolib.GetTDewPointFromRelHum(30.0, 0.80)
+Sat_deg = psychrolib.GetDegreeOfSaturation(30.0, 0.80, 101325)
+Da_density = psychrolib.GetDryAirDensity(30.0, 101325)
+Psy_props = psychrolib.CalcPsychrometricsFromRelHum(30.0, 0.80, 101325)
+
+print(f'TDewPoint: {TDewPoint} C°')
+print(Psy_props)
+
+
+
